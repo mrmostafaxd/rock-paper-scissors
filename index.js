b0338f0d30b2713eb3da74d98546cf731159d281
Mostafa Sabry <mostafa.sabry98@eng-st.cu.edu.eg>
2022-11-25 03:31:18 +0200

    Add function to simulate a 5 rounds game

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const CHOICES = ["Rock", "Paper", "Scissors"]
+const CHOICES = ["rock", "paper", "scissors"]
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * CHOICES.length);
@@ -34,20 +34,57 @@ function playRound(playerChoice, computerChoice) {
     if (winner === 0) {
         message = `You Win! ${playerChoice} beats ${computerChoice}`;
     } else if (winner === 1) {
-        message = `You Lose! ${computerChoice} beats ${playerChoice}`
+        message = `You Lose! ${computerChoice} beats ${playerChoice}`;
     } else {
-        message = `Draw`
+        message = `Draw`;
     }
 
     return message;
 }
 
-const playerChoice = CHOICES[1] //rock
-const computerChoice = getComputerChoice(); //
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+
+        // get player choice and ensure that it is a number
+        let playerInput = prompt('Choose:\n0: Rock, 1: Paper, 2: Scissors');
+        while(playerInput === null || playerInput === '' ||
+         isNaN(playerInput) || playerInput > 2) {
+            alert('Wrong choice');
+            playerInput = prompt('Choose:\n0: Rock, 1: Paper, 2: Scissors');
+        }
+        const playerChoice = CHOICES[playerInput];
+
+        // get computer choice
+        const computerChoice = getComputerChoice();
+
+        // get round result
+        let roundResult = playRound(playerChoice, computerChoice);
+        
+        // display result
+        console.log(roundResult);
+
+        // update scores
+        if  (roundResult.search(/you win/i) >= 0) {
+            playerWins++;
+        } else if (roundResult.search(/you lose/i) >= 0) {
+            computerWins++;
+        }
+
+    }
+
+    // display the game result
+    if (computerWins > playerWins) {
+        console.log('You lost the game!');
+    } else if (computerWins < playerWins) {
+        console.log('You Won the game!');
+    } else {
+        console.log('Draw!');
+    }
+}
 
 console.group();
-console.log(`Player choice: ${playerChoice}`);
-console.log(`Computer choice: ${computerChoice}`);
-console.log(playRound(playerChoice, computerChoice));
+game();
 console.groupEnd();
 
