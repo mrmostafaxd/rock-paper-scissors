41bb2ddea04522f85ebf4bfd89a1e0f933e328bc
Mostafa Sabry <mostafa.sabry98@eng-st.cu.edu.eg>
2022-11-25 02:54:28 +0200

    Add function to play a game round

--- index.js
+++ index.js
@@ -5,4 +5,49 @@ function getComputerChoice() {
     return CHOICES[choice];
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice) {
+    let winner = 0; // 0: player, 1: Computer, 2: draw
+
+    if (playerChoice === computerChoice) { // Draw
+        winner = 2;
+    } else if (playerChoice === CHOICES[0]) { // Rock
+        if(computerChoice === CHOICES[1]) {
+            winner = 1;
+        } else {
+            winner = 0;
+        }
+    } else if (playerChoice === CHOICES[1]) { // Paper
+        if (computerChoice === CHOICES[0]) {
+            winner = 0;
+        } else {
+            winner = 1;
+        }
+    } else if (playerChoice === CHOICES[2]) { // Scissors
+        if (computerChoice === CHOICES[0]) {
+            winner = 1;
+        } else {
+            winner = 0;
+        }
+    }
+
+    let message = null;
+    if (winner === 0) {
+        message = `You Win! ${playerChoice} beats ${computerChoice}`;
+    } else if (winner === 1) {
+        message = `You Lose! ${computerChoice} beats ${playerChoice}`
+    } else {
+        message = `Draw`
+    }
+
+    return message;
+}
+
+const playerChoice = CHOICES[1] //rock
+const computerChoice = getComputerChoice(); //
+
+console.group();
+console.log(`Player choice: ${playerChoice}`);
+console.log(`Computer choice: ${computerChoice}`);
+console.log(playRound(playerChoice, computerChoice));
+console.groupEnd();
+
