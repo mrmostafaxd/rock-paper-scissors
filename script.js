0fe4b26dda065b55a3921d587cf79bf23c55452e
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2023-12-25 19:34:17 +0200

    Add playRound function

--- script.js
+++ script.js
@@ -11,3 +11,50 @@ function getComputerChoice() {
       return 'unknown';
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+
+  switch (playerSelection) {
+    case 'rock':
+      switch (computerSelection) {
+        case 'paper':
+          return 'You Lose! Paper defeats Rock';
+
+        case 'scissors':
+          return 'You Win! Rock beats Scissors';
+
+        default:
+          return 'Draw!';
+      }
+
+    case 'paper':
+      switch (computerSelection) {
+        case 'scissors':
+          return 'You Lose! Scissors defeat Paper';
+
+        case 'rock':
+          return 'You Win! Paper beats Rock';
+
+        default:
+          return 'Draw!';
+      }
+
+    case 'scissors':
+      switch (computerSelection) {
+        case 'rock':
+          return 'You Lose! Rock defeats Scissors';
+
+        case 'paper':
+          return 'You Win! Scissors beat Paper';
+
+        default:
+          return 'Draw!';
+      }
+
+    default:
+      if (playerSelection === computerSelection) {
+        return 'Draw!';
+      }
+  }
+}
