b41dad77de039883fd2eb8502f823bdd41822898
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-02-28 02:02:58 +0200

    Refactor functions

--- script.js
+++ script.js
@@ -30,67 +30,6 @@ controlsContainer.addEventListener('click', (evt) => {
 });
 
 /* functions */
-function displayScores() {
-  const disPlayerScore = document.querySelector('#player-score');
-  disPlayerScore.textContent = `You: ${playerScore}`;
-
-  const disComputerScore = document.querySelector('#computer-score');
-  disComputerScore.textContent = `Computer: ${computerScore}`;
-}
-
-function displaySelections(playerSelection, computerSelection) {
-  const disPlayerSelection = document.querySelector('#player-move');
-  disPlayerSelection.textContent = reactions[playerSelection];
-
-  const disComputerSelection = document.querySelector('#computer-move');
-  disComputerSelection.textContent = reactions[computerSelection];
-}
-
-function displayResult(resultType) {
-  const disResultMessage = document.querySelector('#result');
-
-  disResultMessage.classList.remove('win');
-  disResultMessage.classList.remove('lose');
-
-  switch (resultType) {
-    case 'win':
-      disResultMessage.textContent = 'YOU WIN !!';
-      disResultMessage.classList.add('win');
-      break;
-
-    case 'lose':
-      disResultMessage.textContent = 'YOU LOSE !!';
-      disResultMessage.classList.add('lose');
-      break;
-    case 'reset':
-    default:
-      disResultMessage.textContent = '\xa0';
-  }
-}
-
-function getComputerSelection() {
-  const randomNumber = Math.floor(Math.random() * 3);
-  switch (randomNumber) {
-    case 0:
-      return 'rock';
-    case 1:
-      return 'paper';
-    case 2:
-      return 'scissors';
-    default:
-      return 'unknown';
-  }
-}
-
-function updateScores(value) {
-  if (value > 0) {
-    playerScore += value;
-  } else if (value < 0) {
-    // when the player loses a point, the computer wins a point
-    computerScore += -1 * value;
-  }
-}
-
 function playRound(playerSelection) {
   // reset the previous game result at the start of the current game
   if (playerScore === winScore || computerScore === winScore) {
@@ -107,8 +46,8 @@ function playRound(playerSelection) {
   const resultValue = determineRoundResult(playerSelection, computerSelection);
 
   updateScores(resultValue);
-
   displayScores();
+
   if (playerScore === winScore) {
     displaySelections('winner', 'loser');
     displayResult('win');
@@ -163,25 +102,63 @@ function determineRoundResult(playerSelection, computerSelection) {
   }
 }
 
-function game() {
-  let finalScore = 0;
-  for (let currentRound = 0; currentRound < 5; currentRound++) {
-    const playerSelection = prompt('Enter your move!', '').trim();
-    const computerSelection = getComputerSelection();
-    const roundResult = playRound(playerSelection, computerSelection);
-    finalScore += roundResult.value;
-    console.log(`ROUND ${currentRound + 1}`);
-    console.log(roundResult.message);
-    console.log('');
-  }
-
-  if (finalScore === 0) {
-    console.log('Final Result is: Draw');
-  } else if (finalScore > 0) {
-    console.log('Final Result is Winning');
-  } else {
-    console.log('Final Result is Losing');
+function getComputerSelection() {
+  const randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      return 'unknown';
   }
 }
 
-// game();
+function updateScores(value) {
+  if (value > 0) {
+    playerScore += value;
+  } else if (value < 0) {
+    // when the player loses a point, the computer wins a point
+    computerScore += -1 * value;
+  }
+}
+
+function displayScores() {
+  const disPlayerScore = document.querySelector('#player-score');
+  disPlayerScore.textContent = `You: ${playerScore}`;
+
+  const disComputerScore = document.querySelector('#computer-score');
+  disComputerScore.textContent = `Computer: ${computerScore}`;
+}
+
+function displaySelections(playerSelection, computerSelection) {
+  const disPlayerSelection = document.querySelector('#player-move');
+  disPlayerSelection.textContent = reactions[playerSelection];
+
+  const disComputerSelection = document.querySelector('#computer-move');
+  disComputerSelection.textContent = reactions[computerSelection];
+}
+
+function displayResult(resultType) {
+  const disResultMessage = document.querySelector('#result');
+
+  disResultMessage.classList.remove('win');
+  disResultMessage.classList.remove('lose');
+
+  switch (resultType) {
+    case 'win':
+      disResultMessage.textContent = 'YOU WIN !!';
+      disResultMessage.classList.add('win');
+      break;
+
+    case 'lose':
+      disResultMessage.textContent = 'YOU LOSE !!';
+      disResultMessage.classList.add('lose');
+      break;
+    case 'reset':
+    default:
+      disResultMessage.textContent = '\xa0';
+  }
+}
